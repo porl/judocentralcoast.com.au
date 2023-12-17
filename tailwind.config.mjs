ef922b0c1b1861cab252508e2e04e3ea7cccba6f
porl <porl42@gmail.com>
2023-12-17 17:05:48 +1100

    start work on site structure and dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-            colors: {
-                primary: '#0071ce',
-                secondary: '#ebaa00',
-            },
-        },
-	},
-	plugins: [],
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0071ce',
+        secondary: '#ebaa00',
+        'bg-dark': colors.neutral[900],
+        'bg-light': colors.neutral[200],
+      },
+    },
+  },
+  plugins: [],
 }
